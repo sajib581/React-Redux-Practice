3a5a8eac6174348185dbfdf16aa1b84e3e16056e
sajib581 <sajibdas581@gmail.com>
2020-06-12 00:03:43 +0600

    state edit delete

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,71 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import Example from '../components/Example'
+import Books from './Books/Books'
+import Book from './Books/Book/Book'
 
 class App extends Component {
-  state = {
-    name : ""
-  }
-  clickHandeler = (event) =>{
-    console.log(event.target)    
-  }
-  inputHandeler = (event) => {
-    this.setState({
-      name : event.target.value
-    }) 
-  }  
-    
+  
+state = {
+  books : [
+    {
+      name : 'Javascript',
+      price : 30,
+      id : 1
+    },
+    {
+      name : 'React',
+      price : 20,
+      id : 2
+    },
+    {
+      name : 'Redux',
+      price : 10,
+      id : 3
+    },
+    {
+      name : 'React Native',
+      price : 50,
+      id : 4
+    },
+  ]
+}
+
+deleteHandeler = (id) => {
+  let newState = this.state.books.filter((book)=>{
+    return book.id != id
+  })
+  this.setState({
+    books : newState
+  })
+}
+changeHandeler = (name,id) => {
+  let newBooks = this.state.books.map(book => {
+    if(id==book.id){
+      return {
+        ...book,
+        name
+      }
+    }
+    return book    
+  })
+  this.setState({
+    books : newBooks
+  })
+}
+
   render(){ 
+    
     return (
-      <div className="App">
-        <div class="container my-3">
-        <input onChange={this.inputHandeler} type="text" placeholder="Enter your name"/>
-          <button className= "btn btn-primary" onClick={ (event)=> console.log(event.target) }>
-            Click Me
-          </button> 
-          { this.state.name ? <p>Hello Mr. {this.state.name} </p>  : "" }
+      <div className='App'>
         
-          <Example name="Sajib"/>
-        </div>        
+        {this.state.books.map(book => {
+          return( 
+          <Book changeHandeler = {this.changeHandeler.bind(this)}  
+          deleteHandeler={this.deleteHandeler.bind(this)}  
+          book={book} />
+          )
+        })}
+      
       
       </div>
       
